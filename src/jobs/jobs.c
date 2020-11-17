acc66d587d4931440a39ecbc616635da6dd22bad
afaraji <alegi.bruno1@gmail.com>
2020-11-17 21:01:02 +0100

    working on jobs

--- src/jobs/jobs.c
+++ src/jobs/jobs.c
@@ -64,7 +64,72 @@ The shell maintains a table of jobs. Before every prompt for a command, the shel
 	waitpid( ).
 */
 
-void	job_control(t_and_or *cmd, int bg)
+char	*ft_getsigstr1_12(int sig)
+{
+	static char *str[13];
+	if (!str[0])
+	{
+		str[0] = "done";
+		str[1] = "hangup";
+		str[2] = "Interrupt";
+		str[3] = "Quit";
+		str[4] = "Illegal instruction";
+		str[5] = "trace trap";
+		str[6] = "abort";
+		str[7] = "EMT trap";
+		str[8] = "floating point exception";
+		str[9] = "killed";
+		str[10] = "bus error";
+		str[11] = "segmentation fault";
+		str[12] = "Bad system call";
+	}
+	if (sig >= 0 && sig <= 12)
+		return (str[sig]);
+	return (NULL);
+}
+char	*ft_getsigstr13_31(int sig)
+{
+	static char *str[20];
+	if (!str[0])
+	{
+		str[0] = "broken pipe";
+		str[1] = "Alarm clock";
+		str[2] = "terminated";
+		str[4] = "stoped";
+		str[5] = "stoped";
+		str[8] = "stoped";
+		str[9] = "stoped";
+		str[11] = "Cputime limit exceeded";
+		str[12] = "Filesize limit exceeded";
+		str[13] = "Virtual timer expired";
+		str[14] = "Profiling timer expired";
+		str[17] = "User defined signal 1";
+		str[18] = "User defined signal 2";
+	}
+	if (sig >= 13 && sig <= 31)
+		return (str[sig - 13]);
+	return (NULL);
+}
+char	*ft_strsignal(int sig)
+{
+	if (sig <= 12)
+		return (ft_getsigstr1_12(sig));
+	return (ft_getsigstr13_31(sig));
+}
+
+int		killed_by(int sig)
+{
+	ft_print(STDERR, "killed by signal: %d\n", sig);
+	return (sig);
+}
+
+int		stopped_by(int sig)
+{
+	ft_print(STDERR, "stopped by signal: %d\n", sig);
+	return (sig);
+}
+
+int		job_control(t_and_or *cmd, int bg)
 {
 	int		status;
 	pid_t	pid;
@@ -75,12 +140,12 @@ void	job_control(t_and_or *cmd, int bg)
 		printf("pid[%d] has changed state to [%x]\n", pid, status);
 	}
 	if ((pid = fork()) < 0)
-		return ;
+		return (-1);//print error and exit instad ?!!
 	if (pid == 0)
 	{
 		pid = setsid();
 		if (!bg)
-			tcsetpgrp (STDIN, pid); // STDIN !!!!! dont forget return value
+			tcsetpgrp (STDIN, pid); // dont forget return value !!!
 		signal (SIGINT, SIG_DFL);
 		signal (SIGQUIT, SIG_DFL);
 		signal (SIGTSTP, SIG_DFL);
@@ -93,10 +158,22 @@ void	job_control(t_and_or *cmd, int bg)
 	{
 		setpgid(pid, pid);
 		if (!bg)
-			waitpid(pid, &status, 0);
-		// The shell determines that a stop was sent to the child by looking at the status value
-		signal (SIGTTOU, SIG_IGN);
-		tcsetpgrp (STDIN, getpid()); // STDIN !!!!! dont forget return value
-		// add child as suspended process in process list
+		{
+			if (waitpid(pid, &status, WUNTRACED | WCONTINUED) < 0)
+				return (-2);
+			// The shell determines that a stop was sent to the child by looking at the status value
+			if (WIFEXITED(status))
+				return (WEXITSTATUS(status));
+			if (WIFSIGNALED(status))
+				return (killed_by(WTERMSIG(status)));
+			if (WIFSTOPPED(status))
+				return (stopped_by(WSTOPSIG(status)));
+			signal (SIGTTOU, SIG_IGN);
+			signal (SIGTTIN, SIG_IGN);
+			tcsetpgrp (STDIN, getpid()); // dont forget return value !!!
+			// add child as suspended process in process list
+		}
+
 	}
+	return (0);
 }
