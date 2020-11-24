d04f19524339bf483698b16cf2a805d34d166ca3
Samira Azouakan <sazouaka@e1r3p11.1337.ma>
2020-11-24 19:01:52 +0100

    fc de nasser

--- src/builtins/fc__2.c
+++ src/builtins/fc__2.c
@@ -27,10 +27,8 @@
 int		get_opt_str(int opt[5], char *str)
 {
 	int		i;
-	int		e;
 
 	i = 0;
-	e = 0;
 	while (str[i])
 	{
 		if (str[i] == 's')
@@ -42,10 +40,7 @@ int		get_opt_str(int opt[5], char *str)
 		else if (str[i] == 'r')
 			opt[R_OPT] = 1;
 		else if (str[i] == 'e')
-		{
 			opt[E_OPT] = 1;
-			e = 1;
-		}
 		else
 		{
 			ft_print(stderr, "fc: -%c: invalid option", str[i]);
@@ -53,15 +48,12 @@ int		get_opt_str(int opt[5], char *str)
 		}
 		i++;
 	}
-	if (e)
-		return(2);
 	return (1);
 }
 
 int		get_opt_av(int opt[5], char **av, char **editor)
 {
 	int		i;
-	int		ret;
 
 	i = 1;
 	while (av[i] && av[i][0] == '-')
@@ -71,14 +63,20 @@ int		get_opt_av(int opt[5], char **av, char **editor)
 			ft_print(stderr, "shell: fc: history specification out of range");
 			return (-1);
 		}
-		if ((ret = get_opt_str(opt, &av[i][1])) == 0)
+		if (is_all_digits(&av[i][1]))
+		{
+			i++;
+			break ;
+		}
+		if (get_opt_str(opt, &av[i][1]) == 0)
 			return (-1);
-		if (ret = 2)
+		if (opt[E_OPT] == 1)
 		{
 			if (av[i + 1])
 			{
 				i++;
 				*editor = ft_strdup(av[i]);
+				opt[E_OPT] = 2;
 			}
 			else
 			{
@@ -91,17 +89,90 @@ int		get_opt_av(int opt[5], char **av, char **editor)
 	return (i);
 }
 
-char	*get_first(char **av)
+t_hist	*reverse_list(t_hist *list)//not revers !!!!!
 {
-	if (av && av[0])
+	t_hist	*node;
+
+	node = list;
+	if (reverse == 0)
 	{
-		return
+		while (node)
+		{
+			printf("%s", node->hist_str);
+			node = node->next;
+		}
 	}
+	else
+	{
+		while (node->next)
+			node = node->next;
+		while (node)
+		{
+			printf("%s", node->hist_str);
+			node = node->prec;
+		}
+	}
+}
+
+int		get_index_hist(char *s, int l)
+{
+	if (is_all_digits(s) || (s[0] == '-' && is_all_digits(&s[1])))
+		return (ft_atoi(s));
+}
+
+t_hist	*get_fc_list(char *first, char *last, int r, int l)
+{
+	t_hist	*list;
+	int		first_index;
+	int		last_index;
+
+	first_index = get_index_hist(0, first, l);
+	last_index = get_index_hist(1, last, l);
+	if (r)
+		return (reverse_list(list));
+	return (list);
+}
+
+int		fc_print_list(t_hist *list, int n)
+{
+	if (n)
+	{
+		while (list)
+		{
+			ft_print(STDOUT, "\t\t%s\n", list->hist_str);
+			list = list->next;
+		}
+	}
+	else
+	{
+		while (list)
+		{
+			ft_print(STDOUT, "%d\t%s\n", list->index, list->hist_str);
+			list = list->next;
+		}
+	}
+	return (0);
+}
+
+int		fc_exec(t_hist *list, char *editor)
+{
+	//
 }
 
 int		ft_fc_2(char *f, char *l, int opt[5], char *e)
 {
-	return (0);
+	t_hist	*list;
+
+	list = get_fc_list(f, l, opt[R_OPT], opt[L_OPT]);
+	if (list == NULL)
+	{
+		ft_print(STDOUT, "fc: no events in that range\n");
+		return (1);
+	}
+	if (opt[L_OPT])
+		return (fc_print_list(list, opt[N_OPT]));
+	else
+		return (fc_exec(list, e));
 }
 
 int		ft_fc(char **av)
@@ -115,6 +186,10 @@ int		ft_fc(char **av)
 	i = get_opt_av(opt, av, &editor);
 	if (i == -1)
 		return (i);
+	if (opt[S_OPT])
+	{
+		return (fc_do_s());
+	}
 	if (av[i] && av[i + 1] && av[i + 2])
 	{
 		ft_print(stderr, "shell: fc: too many args\n");
