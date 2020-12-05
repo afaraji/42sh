f84e40ae0ff64423194622e9003dc7f549552c2f
awali-al <48225442+awali-al@users.noreply.github.com>
2020-12-05 12:53:05 +0100

    the function is done

--- src/builtins/ch_prnt_dir.c
+++ src/builtins/ch_prnt_dir.c
@@ -6,11 +6,12 @@
 /*   By: awali-al <awali-al@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/05 02:09:44 by awali-al          #+#    #+#             */
-/*   Updated: 2020/12/05 03:12:32 by awali-al         ###   ########.fr       */
+/*   Updated: 2020/12/05 12:52:32 by awali-al         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../libft/libft.h"
+#include "../../inc/expansion.h"
 #include <stdio.h>
 
 static char	*up_a_parent(char **ret, char *tmp)
@@ -30,37 +31,101 @@ static char	*up_a_parent(char **ret, char *tmp)
 	return (tmp + 2);
 }
 
-char		*get_prnt_dir(char *path)
+static char	*first_str(char *str1, char *str2)
 {
-	char	*buf;
-	char	*tmp;
-	char	*ret;
+	if ((str1 && !str2) || (str1 && str2 && str1 < str2))
+		return (str1);
+	else if ((!str1 && str2) || (str1 && str2 && str1 > str2))
+		return (str2);
+	else
+		return (NULL);
+}
 
-	if (path[0] == '/')
+static char	*new_path(char **ret, char *path)
+{
+	char	*chosen;
+	char	*tmp;
+	char	*buf;
+	int		n;
+
+	chosen = first_str(ft_strstr(path, "/./"), ft_strstr(path, "/../"));
+	buf = !ft_strcmp(*ret, "/") ? ft_strdup(*ret) : ft_strjoin(*ret, "/");
+	ft_strdel(ret);
+	if (chosen)
+	{
+		n = chosen - path;
+		tmp = ft_strsub(path, 0, n);
+		*ret = ft_strjoin(buf, tmp);
+		ft_strdel(&tmp);
+		n = chosen[1] == '/' ? n + 2 : n + 3;
+	}
+	else
+	{
+		*ret = ft_strjoin(buf, path);
+		n = ft_strlen(path) - 1;
+	}
+	ft_strdel(&buf);
+	return (path + n);
+}
+
+// this function needs to be put in a separate file
+
+static int	same_char(char *str, char c)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+		if (str[i++] != c)
+			return (1);
+	return (0);
+}
+
+static char	*parent_dots(char *path, char *pwd)
+{
+	char	*ret;
+	int		i;
+
+	// ret = ft_strdup(var_get_value("PATH", 1)); replaces the following line and you need to remove the pwd from the prototype of the function
+	ret = ft_strdup(pwd);
+	i = 1;
+	while (path[i])
+	{
+		up_a_parent(&ret, "helloo");
+		i++;
+	}
+	return (ret);
+}
+
+char		*get_real_path(char *path, char *pwd)
+{
+	char	*ret;
+	char	*tmp;
+
+	if (*path == '/')
 		return (ft_strdup(path));
-	// ret = ft_strdup(getcwd(NULL, 0)); when trying it with the real deal replace the line below with this one
-	ret = ft_strdup("/usr/bin");
+	if (!same_char(path, '.'))
+		return (parent_dots(path, pwd));
+	// ret = ft_strdup(var_get_value("PATH", 1)); same here
+	ret = ft_strdup(pwd);
 	tmp = path;
-	while (tmp[0] == '.' || tmp[0] == '/')
+	printf("%s             %s\n", ret, tmp);
+	while (*tmp)
 	{
 		if (tmp[0] == '.' && tmp[1] == '.' && tmp[2] == '.')
 			return (NULL);
 		if (ft_strstr(tmp, "../") == tmp)
 			tmp = up_a_parent(&ret, tmp);
+		if (tmp[0] != '.' && tmp[0] != '/')
+			tmp = new_path(&ret, tmp);
 		tmp++;
+		printf("%s             %s\n", ret, tmp);
 	}
-	if (!ft_strcmp(ret, "/") )
-		buf = ft_strdup(ret);
-	else
-		buf = ft_strjoin(ret, "/");
-	ft_strdel(&ret);
-	ret = ft_strjoin(buf, tmp);
-	ft_strdel(&buf);
 	return (ret);
 }
 
 int			main(void)
 {
-	printf("%s\n", get_prnt_dir(".././///.././../Users"));
+	printf("%s\n", get_real_path("......", "/Users/aminewalialami/42sh/trash/42sh_correction/42sh.txts"));
 	return (0);
 }
